f5d557326ec4a0f64437fe1ed4cce6b45226cd37
Dillon Riecke <dillon.riecke@gmail.com>
2022-09-19 16:48:17 -0500

    Preparing to log landmark data

--- Assets/Scripts/FaceMeshMP.cs
+++ Assets/Scripts/FaceMeshMP.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+using Stopwatch = System.Diagnostics.Stopwatch;
+
 namespace Mediapipe.Unity.Tutorial
 {
     public class FaceMeshMP : MonoBehaviour
@@ -13,7 +15,14 @@ namespace Mediapipe.Unity.Tutorial
         [SerializeField] private int _height;
         [SerializeField] private int _fps;
 
+        private CalculatorGraph _graph;
+        private ResourceManager _resourceManager;
+
         private WebCamTexture _webCamTexture;
+        private Texture2D _inputTexture;
+        private Color32[] _pixelData;
+
+
 
         // Start is called before the first frame update
         private IEnumerator Start()
@@ -30,11 +39,63 @@ namespace Mediapipe.Unity.Tutorial
             yield return new WaitUntil(() => _webCamTexture.width > 16);
 
             _screen.rectTransform.sizeDelta = new Vector2(_width, _height);
-            _screen.texture = _webCamTexture;
 
-            var graph = new CalculatorGraph(_configAsset.text);
-            graph.StartRun().AssertOk();
+            _inputTexture = new Texture2D(_width, _height, TextureFormat.RGBA32, false);
+            _pixelData = new Color32[_width * _height];
 
+            _resourceManager = new StreamingAssetsResourceManager();
+            yield return _resourceManager.PrepareAssetAsync("face_detection_short_range.bytes");
+            yield return _resourceManager.PrepareAssetAsync("face_landmark_with_attention.bytes");
+
+            var stopwatch = new Stopwatch();
+
+            _graph = new CalculatorGraph(_configAsset.text);
+            var outputVideoStream = new OutputStream<ImageFramePacket, ImageFrame>(_graph, "output_video");
+            // NOTE: StartPolling returns Status
+            outputVideoStream.StartPolling().AssertOk();
+            _graph.StartRun().AssertOk();
+
+            stopwatch.Start();
+
+            var screenRect = _screen.GetComponent<RectTransform>().rect;
+
+            while (true)
+            {
+                _inputTexture.SetPixels32(_webCamTexture.GetPixels32(_pixelData));
+                var imageFrame = new ImageFrame(ImageFormat.Types.Format.Srgba, _width, _height, _width * 4, _inputTexture.GetRawTextureData<byte>());
+                var currentTimestamp = stopwatch.ElapsedTicks / (System.TimeSpan.TicksPerMillisecond / 1000);
+                _graph.AddPacketToInputStream("input_video", new ImageFramePacket(imageFrame, new Timestamp(currentTimestamp))).AssertOk();
+
+                yield return new WaitForEndOfFrame();
+
+                var outputTexture = new Texture2D(_width, _height, TextureFormat.RGBA32, false);
+                var outputPixelData = new Color32[_width * _height];
+                _screen.texture = outputTexture;
+
+                if (outputVideoStream.TryGetNext(out var outputVideo))
+                {
+                    if (outputVideo.TryReadPixelData(outputPixelData))
+                    {
+                        outputTexture.SetPixels32(outputPixelData);
+                        outputTexture.Apply();
+                    }
+                }
+
+                var multiFaceLandmarkStream = new OutputStream<NormalizedLandmarkListVectorPacket, List<NormalizedLandmarkList>>(_graph, "multi_face_landmarks");
+                multiFaceLandmarkStream.StartPolling().AssertOk();
+
+                if (multiFaceLandmarkStream.TryGetNext(out var multiFaceLandmarks))
+                {
+                    if (multiFaceLandmarks != null && multiFaceLandmarks.Count > 0)
+                    {
+                        foreach(var landmarks in multiFaceLandmarks)
+                        {
+                            var topOfHead = landmarks.Landmark[10];
+                            Debug.Log($"Coordinates: {topOfHead.ToString()}");
+                        }
+                    }
+                }
+            }
 
         }
 
@@ -43,6 +104,19 @@ namespace Mediapipe.Unity.Tutorial
             if (_webCamTexture != null)
             {
                 _webCamTexture.Stop();
+            }
+
+            if (_graph != null)
+            {
+                try
+                {
+                    _graph.CloseInputStream("input_video").AssertOk();
+                    _graph.WaitUntilDone().AssertOk();
+                }
+                finally
+                {
+                    _graph.Dispose();
+                }
             }
         }
 
